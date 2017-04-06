a904400a2054b9b6ba69e54072e116ee4cdbd084
Ubuntu <rooty@mailserver.s0stgakqb2ju1ifzv343gza4hb.yx.internal.cloudapp.net>
2017-04-06 16:42:32 +0000

    Added commenting, cleaned up existing comments

--- pyphish.py
+++ pyphish.py
@@ -1,3 +1,24 @@
+"""Py-Phished v1.0
+Ben Floyd, License: BSD-3 Clause
+Py-Phished is a python-based webserver/email client for internal phishing campaigns.
+
+The goal is to test company training efficacy and email safety and assess the overall response
+rate to malicious phishing attempts.  The emailer can be controlled via the site with /start and
+/stop commands.  Base64 encoded email addresses are sent to "victims" as part of the link for 
+tracking purposes.
+
+Inputs: csv file with email, first name, last name
+Usage:  http://url/start
+        http://url/stop
+Output: caught.csv
+
+
+
+In future versions, metrics are planned.
+"""
+
+
+
 import sys
 import threading
 import tornado.ioloop
@@ -8,27 +29,25 @@ import base64
 import time
 import signal
 
-#filename = ".\\index.html"
+
+
 phish_f = "phish.html"
 targets = "targets.csv"
-#ph_msg = ".\\ph_msg.txt"
-#fin = open(filename, 'rb').read()
 pin = open(phish_f, 'rb').read()
 tin = open(targets, 'rb')
 csv_r = csv.DictReader(tin, dialect='excel')
-#row_count = len(csv_r)
 disp = {}
 db = {}
 caught = {}
 i = 0
 j = 0
+
+#Return address
 fr_addr = 'accounts@alitheia.com'
 to_addr = ''
-#msg = open(ph_msg, 'rb').read()
 ph_link = ''
-event = 0
 
-
+#Handle CTRL+C (linux only)
 def signal_handler(signal, frame):
 	print('Caught %s' % signal)
 	print "Stopping phishing campaign..."
@@ -38,12 +57,8 @@ def signal_handler(signal, frame):
         print "Asked Tornado to exit"
 	sys.exit(0)
 
+#Mass emailer.  Set to stagger 100 emails with a 10s pause
 class phish_campaign:
-	#global db
-        #global disp	
-	#target = {}
-	#csvfile = open('targets.csv', 'rb')
-	#reader = csv.DictReader(csvfile, dialect='excel')
 
 	def __init__(self):
 		target = {}
@@ -76,6 +91,7 @@ class phish_campaign:
   	               	j += 1
 
 
+#Called by "phish_campaign"
 def testmail(target):
 
 	# Send an HTML email with an embedded image and a plain text message for
@@ -125,7 +141,7 @@ def testmail(target):
 	#smtp.quit()
 
 
-
+#Populate a python dict with the contents of "targets.csv"
 def pop_csv():
 	i = 0
 	global reader
@@ -136,71 +152,36 @@ def pop_csv():
 		#print row['Email'], row['First Name']
 		db[i] = {"Email": row['Email'], "F_name": row['First Name']}
 		i += 1
-		# while rownum < rowtot:
-		# print rownum
-		# if rownum == 0:
-			# print csv
-			# rownum += 1
-	# rownum = csv_r.line_num()
-	# print ("Csv line number is %d" % rownum)
-	# '''rownum = 0
-	# for row in csv_r:
-	# # Save header row.
-		# if rownum ==0:
-			# header = row
-			# cols = row.split(",")
-			# disp[rownum] = [str(item) for item in row]
-		# else:
-			# colnum = 0
-			# for col in row:
-				# disp[rownum,colnum] = col
-				# colnum += 1
-				# rownum += 1'''
-	#print(disp)
 	print 'Phishing targets populated.'
-		#print 'Dear %s,\n\tWe regret to inform you that you have been phished.\n\n%s' % (disp[2]["F_name"], base64.b64encode(disp[2]["Email"]))
 
 
-
+#Tornado web handler for root "/" request
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-		#self.write(csv_r)
-		#self.write(disp)
 		self.write('<html>Welcome to Alitheia\'s website.  Undergoing Maintenance....</html>')
-		#testmail()
 	
 
+#Tornado web handler for "/start" request
 class StartHandler(tornado.web.RequestHandler):
 	def get(self):
-		#try:
-		#	tp = threading.Thread(target=phish_campaign)
-	        #	tp.start()  
-		#except (KeyboardInterrupt, SystemExit):
-		#	tp.stop()
-		#	sys.exit()
 		global e
 		e.clear()
 		print "Web Request:  Starting Phishing emails..."
 		pc = threading.Thread(target=phish_campaign)
 		pc.start()
 
+#Tornado web handler for "/stop" request
 class StopHandler(tornado.web.RequestHandler):
     #def initialize(self, db):
         #self.db = db
 	def get(self):
-		#self.write("Stopping phishing campaign...")
-		#ioloop = tornado.ioloop.IOLoop.instance()
-		#ioloop.add_callback(ioloop.stop)
 		global e
 		e.set()
 		print "Web Request: Stopping phishing emails..."
-		#print "Caught SIGKILL"		
-		#import os,signal
-		#os.kill(os.getpid(), signal.SIGKILL)
-		#app = Application([
-		#url(r"/", MainHandler),
-		#url(r"/stop", StopHandler)
-    #])
+
+
+#Tornado web handler for "/account/<base64encoded_email>"
+#Logs incoming, successful phishing attempts
 class AccountHandler(tornado.web.RequestHandler):
     def get(self, ph_id):
 		print ph_id
@@ -217,12 +198,9 @@ class AccountHandler(tornado.web.RequestHandler):
 		with open('targets.csv') as csvfile:
 			reader = csv.DictReader(csvfile)
 			for row in reader:
-				#print row['Email'], row['First Name']
-				#disp[i] = {"Email": row['Email'], "F_name": row['First Name']}
 				if row['Email'] == test:
 					buf = row['Email'] + ', ' +  row['First Name'] + ', ' + row['Last Name'] + '\n'
 					fp.write(buf)
-					#self.write(row['Email'])
 					self.write(pin)
 					caught[i] = {"Email": row['Email'], "F_name": row['First Name'], "L_name": row['Last Name']}
 					i += 1
@@ -230,7 +208,7 @@ class AccountHandler(tornado.web.RequestHandler):
 		
 		#print "Total people phished: " + len(caught)
 	
-			
+#Defining handlers for various web requests in Tornado			
 def make_app():
     return tornado.web.Application([
 		(r"/", MainHandler),
@@ -240,34 +218,20 @@ def make_app():
     ])
 
 
-
+#define listen port here (default is 80)
 def startTornado():
 	app = make_app()
 	app.listen(80)
 	tornado.ioloop.IOLoop.instance().start()
 	
 def stopTornado():
-    #ioloop = tornado.ioloop.IOLoop.instance()
-    #ioloop.add_callback(ioloop.stop)
-    #print "Asked Tornado to exit"
-    #sys.exit(0)
 	import os,signal
 	os.kill(os.getpid(), signal.SIGINT)
-#def target():
-#	rownum = 0
-#	print ("Csv line number is " %s, csv_r.line_num())
-#	'''for row in csv_r:
-#		# Save header row.
-#		if rownum ==0:
-#			header = row
-#		else:
-#			colnum = 0
-#			for col in row:
-#				print '%-8s: %s' % (header[colnum], col)
-#				colnum += 1
-#				rownum += 1'''
 				
 
+#start threaded Tornado instance on port 80
+#handle web requests
+#let Tornado take it from there.
 def main():
 	print("Starting web server...")
 	#pop_csv()
@@ -275,8 +239,6 @@ def main():
 	e = threading.Event()
 	t = threading.Thread(target=startTornado) 
 	t.start()
-	#tp = threading.Thread(target=phish_campaign)
-	#tp.start()
 	pop_csv()
 
 if __name__ == "__main__":
